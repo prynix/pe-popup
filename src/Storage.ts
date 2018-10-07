165aeb2eeec56090bec1d585b7afb39c9afeaf29
cr7parker <romalezhnin@gmail.com>
2018-10-07 12:43:51 +0700

    ===

--- src/Storage.ts
+++ src/Storage.ts
@@ -17,7 +17,7 @@ export default class Storage {
 
 
   public canShowPopup(): boolean {
-    return this.counter >= Storage.deep(1, 3);
+    return this.counter === Storage.deep(1, 2);
   }
 
   public incrementShowCounter(): void {
